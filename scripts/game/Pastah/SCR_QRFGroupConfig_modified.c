a6e83c44e61d4a9301f0202ad7eae2a55ba5f63b
Davidj361 <david.j.361@gmail.com>
2026-05-05 05:16:44 -0400

    WIP modded QRF Dispacher, WIP HradSandwich mission

---
+++ scripts/game/Pastah/SCR_QRFGroupConfig_modified.c
@@ -0,0 +1,48 @@
+class SCR_QRFGroupConfig_modified : ScriptAndConfig
+{
+	[Attribute(desc : "Group/unit prefab")] protected ResourceName m_sGroupPrefab; // (f.e. {E552DABF3636C2AD}Prefabs/Groups/OPFOR/Group_USSR_RifleSquad.et),
+
+	[Attribute(SCR_EQRFGroupType.INFANTRY.ToString(), UIWidgets.ComboBox, desc : "Type of this group which will be used to determine if they can spawn for provided spawnpoint", enums : ParamEnumArray.FromEnum(SCR_EQRFGroupType))] protected SCR_EQRFGroupType m_eGroupType; // (INFANTRY/VEHICLE/AIR),
+
+	[Attribute("1", UIWidgets.Auto, "Cost of spawning such QRF group")] protected int m_iSpawnCost; // (0/1/2/3...),
+
+	[Attribute("-1", UIWidgets.Auto, "How many times this QRF unit can be used until we will run out of them where -1 == unlimited")] protected int m_iNumberOfAvailableGroups; // (0 unlimited, 1/2/3...)
+
+	[Attribute("0", UIWidgets.Auto, "Delay for spawning this same QRF, stops vehicles spawning ontop of each other")] protected float m_fQRFSpawnDelay;
+
+	//------------------------------------------------------------------------------------------------
+	ResourceName GetGroupPrefabName()
+	{
+		return m_sGroupPrefab;
+	}
+
+	//------------------------------------------------------------------------------------------------
+	SCR_EQRFGroupType GetGroupType()
+	{
+		return m_eGroupType;
+	}
+
+	//------------------------------------------------------------------------------------------------
+	int GetSpawnCost()
+	{
+		return m_iSpawnCost;
+	}
+
+	//------------------------------------------------------------------------------------------------
+	int GetNumberOfAvailableGroups()
+	{
+		return m_iNumberOfAvailableGroups;
+	}
+
+	//------------------------------------------------------------------------------------------------
+	void SetNumberOfAvailableGroups(int newNumber)
+	{
+		m_iNumberOfAvailableGroups = newNumber;
+	}
+
+	//------------------------------------------------------------------------------------------------
+	float GetQRFSpawnDelay()
+	{
+		return m_fQRFSpawnDelay;
+	}
+}
